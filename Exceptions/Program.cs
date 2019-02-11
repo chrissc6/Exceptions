91c8bf28e76432636b7192b8e139ac2d80a36bf9
Chris Caudill <christopherscaudill@gmail.com>
2019-02-11 16:04:20 -0500

    init-c

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,6 +10,44 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            string str = null;
+
+            try
+            {
+                var answer = GetErrorMessage(str);
+                ArrayError(5);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message + " Error 322, Null, Str cannot be null");
+                str = string.Empty;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message + " Error 321, ArrayIndex, No element in index");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message + " Error 999, General exception error");
+            }
+
+        }
+
+
+        static string ArrayError(int idx)
+        {
+            string[] messages =
+            {
+                "message1",
+                "message2",
+                "message3"
+            };
+            return messages[idx];
+        }
+
+        static string GetErrorMessage(string str)
+        {
+            return str.ToLower();
         }
     }
 }
